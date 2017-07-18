c6b91706c9f825b1f4e9ab06a5ed4ce525a87184
wk645 <wk645@nyu.edu>
2017-07-18 08:38:30 -0400

    food

--- app.rb
+++ app.rb
@@ -8,4 +8,9 @@ class App < Sinatra::Base
 
   # Add your post route and action below
 
+  post '/food' do
+  	# params.to_s
+  	"My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
+
 end
